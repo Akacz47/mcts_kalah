117858fe4269366587baa8f1d43f7a41437febac
Світлана Жук <claire.zhuk@gmail.com>
2025-12-03 10:02:39 +0100

    parametry

--- src/main_simple.py
+++ src/main_simple.py
@@ -1,11 +1,11 @@
 import numpy as np
 from mcts import MCTS
 from c4 import C4
-from game_runner_cpu import GameRunner
+from game_runner import GameRunner
 from kalah import Kalah
  
 if __name__=="__main__":
-    AI = MCTS(search_time_limit=0.1, search_steps_limit=np.inf, vanilla=False)
+    AI = MCTS(search_time_limit=5, search_steps_limit=np.inf, vanilla=True)
     #game_runner = GameRunner(C4,None,AI,0,1,None)
-    game_runner = GameRunner(Kalah,AI,AI,0,1,None) # None, AI
+    game_runner = GameRunner(Kalah,AI,None,0,1,None) # None, AI
     outcome,game_info = game_runner.run()
